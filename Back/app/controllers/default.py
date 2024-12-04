82da6fa0f2d3c8023d513144d9462a06d76d2f6b
Fabio Gabriel <fbogabriel@outlook.com>
2024-12-04 10:41:44 -0300

    Feito novas rotas para obter anuncios, e perfil

--- Back/app/controllers/default.py
+++ Back/app/controllers/default.py
@@ -1,6 +1,8 @@
-from flask import jsonify, request #Usado para retornar paginas HTML, ocioso ainda
+from flask import jsonify, request, session #Usado para retornar paginas HTML, ocioso ainda
 from app import app, db
 from app.models import *
+
+app.secret_key = "chave_secreta_com_Cruzeiro"
 
 @app.route("/")
 def teste():
@@ -22,24 +24,69 @@ def login():
     email = dados.get('cpf')
     senha =  dados.get('senha')
 
+    session['user_id'] = None
     user = Perfil.query.filter_by(email=email).first()
     if user and user.senha == senha:
-        return jsonify({"login":"true"})
+        session['user_id'] = user.id
+        
+        dados = {}
+        dados["email"] = user.email
+        dados["nome"] = user.nome
+        dados["curso"] = user.curso
+        dados["reputacao"] = user.reputacao
+
+        dados["login"] = "true" #Temporario para nao quebrar aplicacao
+
+        return jsonify(dados)
     
     #insert into users(email, name, password) values("fabio@gabriel.com","Fabio","cruzeiro");
     
     return jsonify({"login":"false"})
 
-@app.route("/teste1")
-def teste1():
-    db.session.add(Perfil("lmeato@id.uff.br", "1234", "Leo", "CC", 5))
-    db.session.commit()
-    return "ok"
+@app.route("/logout")
+def logout():
+    user_id = session["user_id"]
+    if user_id:
+        session.pop(user_id, None)
+        return jsonify({"status", "deslogado"})
+    return jsonify({"status", "Ninguem cadastrado"})
 
-@app.route("/teste2")
-def teste2():
-    s = Perfil.query.filter_by(nome="Leo").first()
-    curso = s.curso
-    return curso
+# @app.route("/teste1")
+# def teste1():
+#     db.session.add(Perfil("lmeato@id.uff.br", "1234", "Leo", "CC", 5))
+#     db.session.commit()
+#     return "ok"
+
+# @app.route("/teste2")
+# def teste2():
+#     s = Perfil.query.filter_by(nome="Leo").first()
+#     curso = s.curso
+#     return curso
+
+
+@app.route("/anuncios_geral")
+def anuncios_geral():
+    dados = {}
+    if session['user_id']:
+        anuncios_pessoais = Anuncio.query().all()
+        
+        #Nao sei se funciona retornar a linha inteira
+        dados["dados"] = anuncios_pessoais
+        return jsonify(dados)
+    dados["dados"] = None
+    return jsonify(dados)
+
+@app.route("/anuncios_pessoais")
+def anuncios_pessoais():
+    dados = {}
+    if session['user_id']:
+        anuncios_pessoais = Anuncio.query.filter_by(id=session["user_id"])
+
+        #Nao sei se funciona retornar a linha inteira
+        dados["dados"] = anuncios_pessoais
+        return jsonify(dados)
+    dados["dados"] = None
+    return jsonify(dados)
+
 
 #Fazer aqui o controle das requisicoes do flutter
--- Back/run.py
+++ Back/run.py
@@ -2,4 +2,4 @@ from app import app
 
 if __name__ == "__main__":#Feito para rodar a aplicacao
     #db.create_all()
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, threaded=True, debug=True)
