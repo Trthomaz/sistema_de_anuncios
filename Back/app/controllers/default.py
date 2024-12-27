263fae990073d0cc6e38a8462216dedd19193849
Fabio Gabriel <fbogabriel@outlook.com>
2024-12-27 08:36:25 -0300

    Removido duplicada de rotas.

--- Back/app/controllers/default.py
+++ Back/app/controllers/default.py
@@ -76,6 +76,7 @@ def criar_anuncio():
     user_id = dados.get("user_id")
     anuncio = Anuncio(user_id,titulo,descricao,celular,cep,categoria,ativo=ativo,tipo=tipo_anuncio, preco=preco)
     try:
+        #Perfil().add_anuncio(anuncio)
         perfil = Perfil.query.filter_by(id=user_id).first()
         perfil.add_anuncio(anuncio)
         return jsonify({"status":True})
