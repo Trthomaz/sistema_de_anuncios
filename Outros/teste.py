6fe1ffedb474a9ff81c3c2f790daf63abcdd9b37
LEONARDO FERNANDES <leomeato@gmail.com>
2025-01-11 10:40:29 -0300

    #1 semanas 10-12

--- Outros/teste.py
+++ Outros/teste.py
@@ -2,14 +2,30 @@ import datetime
 from time import sleep
 from random import shuffle
 
-def sort_by_date(objs):
-    n = len(objs)
-    for i in range (n):
-        for j in range (n - 1):
-            if objs[j].date > objs[j + 1].date:
-                aux = objs[j]
-                objs[j] = objs[j+1]
-                objs[j+1] = aux
+def sort_by_date(objs, type):
+    if type == "bubble":
+        n = len(objs)
+        for i in range (n):
+            for j in range (n - 1):
+                if objs[j].date > objs[j + 1].date:
+                    objs[j], objs[j+1] = objs[j+1], objs[j]
+        return objs
+    elif type == "quick":
+        return quick_sort(objs)
+
+def quick_sort(lista):
+    if len(lista) <= 1:
+        return lista
+
+    pivo = lista[0]
+    lista1 = []
+    lista2 = []
+    for v in lista[1::]:
+        if v.date > pivo.date:
+            lista2.append(v)
+        else:
+            lista1.append(v)
+    return quick_sort(lista1) + [pivo] + quick_sort(lista2)
 
 
 class Obj():
@@ -20,7 +36,7 @@ objs = []
 
 for i in range(10):
     objs.append(Obj())
-    sleep(0.2)
+    sleep(0.1)
 
 shuffle(objs)
 
@@ -29,7 +45,7 @@ for v in objs:
 
 print()
 
-sort_by_date(objs)
+objs = sort_by_date(objs, "bubble")
 
 for v in objs:
     print(v.date)
