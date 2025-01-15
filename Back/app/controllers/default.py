65fb81ce105eb0141c08abd0a4b409c10781304f
LEONARDO FERNANDES <leomeato@gmail.com>
2025-01-15 01:01:20 -0300

    .

--- Back/app/controllers/default.py
+++ Back/app/controllers/default.py
@@ -341,14 +341,15 @@ def avaliar():
     if anuncio.nota:
         return jsonify({"dados": {"msg": "Notas já foram dadas"}})
     transacao = Transacao.query.filter_by(anuncio = a_id).first()
-    perfil = Perfil.query.filter_by(id=id).first()
-    p_a = Perfil.query.filter_by(id=anuncio.anunciante).first()
+    #perfil = Perfil.query.filter_by(id=id).first()
+    p_a = Perfil.query.filter_by(id=transacao.anunciante).first()
+    p_i = Perfil.query.filter_by(id=transacao.interessado).first()
     if id == transacao.interessado:
         transacao.add_nota_interessado(nota)
         p_a.att_reputação()
     elif id == anuncio.anunciante:
         transacao.add_nota_anunciante(nota)
-        perfil.att_reputacao()
+        p_i.att_reputacao()
     else:
         return jsonify({"dados": {"msg": "vixe mano kkk de quem que é esse id aí vei...."}})
     if (transacao.nota_interessado is not None) and (transacao.nota_anunciante is not None):
